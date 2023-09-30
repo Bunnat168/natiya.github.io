aa02788d08a2fa706dbb883795d7347fcebba507
Bunnat168 <146540131+Bunnat168@users.noreply.github.com>
2023-09-30 18:40:23 +0700

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,17 @@
+function txtStyle(){
+    text.style.background = "#aec529b7";
+    text.style.color = "black";
+    text.style.fontSize = "75px";
+    text.style.borderRadius = "30px 0";
+    text.style.boxShadow = "0 0 10px red";
+    text.style.padding = "10px";
+
+}
+function RetxtStyle(){
+    text.style.background = "";
+    text.style.color = "";
+    text.style.fontSize = "";
+    text.style.borderRadius = "";
+    text.style.boxShadow = "";
+    text.style.padding = "";
+}
